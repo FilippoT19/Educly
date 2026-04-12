a6d5615c74ae68bde5652eccf89c8de8c8dec7d7
FilippoT19 <ftopinelli@gmail.com>
2026-04-12 16:37:07 +0200

    homepage edit2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
             Educly
           </h1>
           <p className="text-[18px] sm:text-[20px] text-muted-foreground leading-relaxed">
-            L&apos;Il futuro dell'educazione.
+            Il futuro dell&apos;Ieducazione.
           </p>
         </div>
 
